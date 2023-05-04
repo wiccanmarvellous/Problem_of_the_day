10c616b05ac227a6b37c80c3fb4664d5c7b7fa0d
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-04 22:40:58 +0530

    4 may 2023

---
+++ 2023_5_4_leetcode_649_Dota2_Senate.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rq, dq;
+        
+        for(int i=0; i<senate.size(); i++)
+            senate[i] == 'R' ? rq.push(i): dq.push(i);
+        
+        int a, b;
+        while(!rq.empty() and !dq.empty()){
+            a = rq.front(), b = dq.front();
+            rq.pop(), dq.pop();
+            if (a < b) // taking min and pushing it to queue again
+                rq.push(a+senate.size());
+            else
+                dq.push(b+senate.size());
+        }
+        
+		if (rq.size() > dq.size())
+			return "Radiant";
+		else
+			return "Dire";
+    }
+};
+
+int main()
+{
+    string s = "RDD";
+    Solution obj;
+    cout << obj.predictPartyVictory(s) << endl;
+    return 0;
+}
