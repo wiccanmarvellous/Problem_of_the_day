a512e0ed205b197e08b027f5b3d842578bccd4cb
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-10 23:33:16 +0530

    .

--- 2023_5_9_leetcode_54_Spiral_Matrix.cpp
+++ 2023_5_9_leetcode_54_Spiral_Matrix.cpp
@@ -1,36 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+// class Solution
+// {
+// public:
+//     vector<int> spiralOrder(vector<vector<int>> &matrix)
+//     {
+//         vector<vector<int>> dirs{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//         vector<int> res;
+//         int nr = matrix.size();
+//         if (nr == 0)
+//             return res;
+//         int nc = matrix[0].size();
+//         if (nc == 0)
+//             return res;
+
+//         vector<int> nSteps{nc, nr - 1};
+
+//         int iDir = 0;        // index of direction.
+//         int ir = 0, ic = -1; // initial position
+//         while (nSteps[iDir % 2])
+//         {
+//             for (int i = 0; i < nSteps[iDir % 2]; ++i)
+//             {
+//                 ir += dirs[iDir][0];
+//                 ic += dirs[iDir][1];
+//                 res.push_back(matrix[ir][ic]);
+//             }
+//             nSteps[iDir % 2]--;
+//             iDir = (iDir + 1) % 4;
+//         }
+//         return res;
+//     }
+// };
+
+class Solution
+{
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<vector<int> > dirs{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        vector<int> res;
-        int nr = matrix.size();     if (nr == 0) return res;
-        int nc = matrix[0].size();  if (nc == 0) return res;
-        
-        vector<int> nSteps{nc, nr-1};
-        
-        int iDir = 0;   // index of direction.
-        int ir = 0, ic = -1;    // initial position
-        while (nSteps[iDir%2]) {
-            for (int i = 0; i < nSteps[iDir%2]; ++i) {
-                ir += dirs[iDir][0]; ic += dirs[iDir][1];
-                res.push_back(matrix[ir][ic]);
+    vector<int> spiralOrder(vector<vector<int>> &matrix)
+    {
+        vector<int> arr;
+        int i, j, t = 0, b = matrix.size() - 1, l = 0, r = matrix[0].size() - 1;
+        while (t <= b && l <= r)
+        {
+            for (i = t, j = l; j <= r; j++)
+                arr.push_back(matrix[i][j]);
+            t++;
+            for (j = r, i = t; i <= b; i++)
+                arr.push_back(matrix[i][j]);
+            r--;
+            if (t <= b)
+            {
+                for (i = b, j = r; j >= l; j--)
+                    arr.push_back(matrix[i][j]);
+                b--;
             }
-            nSteps[iDir%2]--;
-            iDir = (iDir + 1) % 4;
+
+            if (l <= r)
+            {
+                for (j = l, i = b; i >= t; i--)
+                    arr.push_back(matrix[i][j]);
+                l++;
+            }
         }
-        return res;
+        return arr;
     }
 };
 
 int main()
 {
-    vector<vector<int>> v{{1,2,3},{4, 5, 6},{7,8,9}};
+    vector<vector<int>> v{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     Solution obj;
     vector<int> ans = obj.spiralOrder(v);
-    for (auto it: ans)
+    for (auto it : ans)
         cout << it << ' ';
     cout << endl;
     return 0;
