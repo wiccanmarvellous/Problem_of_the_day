c52db9fc09329d5a67f64f93b42b993a6803c174
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-16 00:44:14 +0530

    15 may 23

--- 2023_05_15_leetcode_1721_Swapping_Nodes_in_a_Linked_List.cpp
+++ 2023_05_15_leetcode_1721_Swapping_Nodes_in_a_Linked_List.cpp
@@ -16,20 +16,79 @@ struct ListNode
 class Solution
 {
 public:
+    ListNode *insertion(ListNode *head)
+    {
+        ListNode *temp = head;
+        cout << "Enter data in linked list and press -1 to stop:-" << endl;
+        while (true)
+        {
+            int d;
+            cin >> d;
+            if (d == -1)
+                break;
+            ListNode *newnode = new ListNode;
+            newnode->val = d;
+            if (head == NULL)
+                head = newnode;
+            else
+                temp->next = newnode;
+            temp = newnode;
+        }
+        temp->next = NULL;
+        cout << endl;
+        return head;
+    }
+
     ListNode *swapNodes(ListNode *head, int k)
     {
-        ListNode *n1 = nullptr, *n2 = nullptr;
-        for (auto p = head; p != nullptr; p = p->next)
+        ListNode *temp = head;
+        int n = 0, i = 0;
+        while (temp != NULL)
         {
-            n2 = n2 == nullptr ? nullptr : n2->next;
-            if (--k == 0)
-            {
-                n1 = p;
-                n2 = head;
-            }
+            n++;
+            temp = temp->next;
         }
-        // The problem description specifically asks to swap values, not nodes themselves.
-        swap(n1->val, n2->val);
+        vector<vector<int>> arr(n, vector<int>(2));
+        temp = head;
+        while (temp != NULL)
+        {
+            arr[i][0] = (temp->val);
+            arr[n - 1 - i][1] = (temp->val);
+            i++;
+            temp = temp->next;
+        }
+
+        temp = head;
+        i = 1;
+        while (temp != NULL)
+        {
+            if (i == k)
+                temp->val = arr[k - 1][1];
+            else if (i == (n + 1 - k))
+                temp->val = arr[k - 1][0];
+            i++;
+            temp = temp->next;
+        }
+
         return head;
     }
 };
+
+int main()
+{
+    Solution obj;
+    ListNode *head = NULL;
+    int k;
+    head = obj.insertion(head);
+    cout << "Enter node to be swapped: ";
+    cin >> k;
+    head = obj.swapNodes(head, k);
+    ListNode *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << ' ';
+        temp = temp->next;
+    }
+    cout << endl;
+    return 0;
+}
