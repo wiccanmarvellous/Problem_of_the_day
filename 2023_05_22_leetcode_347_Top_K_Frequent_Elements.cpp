77a203c55e634708bc064b9990ca1a1160bbabac
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-22 18:44:50 +0530

    feat: create 2023_05_22_leetcode_347_Top_K_Frequent_Elements.cpp

---
+++ 2023_05_22_leetcode_347_Top_K_Frequent_Elements.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        vector<int> ans;
+        int n = nums.size(), num;
+        for (int i = 0; i < n; i++)
+            hash[nums[i]]++;
+        while (k--) {
+            int mFreq = 0;
+            for (auto it : hash) {
+                if (mFreq < it.second) {
+                    mFreq = it.second;
+                    num = it.first;
+                }
+            }
+            ans.push_back(num);
+            hash.erase(num);
+        }
+        return ans;
+    }
+};
