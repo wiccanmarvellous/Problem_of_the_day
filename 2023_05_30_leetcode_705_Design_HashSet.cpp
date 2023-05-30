a27592b286cf28c8c2c4917cd82a40b9be8c69c3
Shreyash <shreyashyaduvanshi@gmail.com>
2023-05-30 20:26:58 +0530

    feat: create 2023_05_30_leetcode_705_Design_HashSet.cpp

---
+++ 2023_05_30_leetcode_705_Design_HashSet.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyHashSet
+{
+public:
+    vector<bool> hash;
+    MyHashSet()
+    {
+        hash.resize(1e6 + 1, false);
+    }
+
+    void add(int key)
+    {
+        hash[key] = true;
+    }
+
+    void remove(int key)
+    {
+        hash[key] = false;
+    }
+
+    bool contains(int key)
+    {
+        return hash[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
