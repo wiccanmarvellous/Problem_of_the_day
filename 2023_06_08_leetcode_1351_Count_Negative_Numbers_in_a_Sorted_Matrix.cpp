c415c39a2ba1ade29bbf6f17952b1ec0ec0f1dbf
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-08 07:57:07 +0530

    update 2023_06_08_leetcode_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp

--- 2023_06_08_leetcode_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
+++ 2023_06_08_leetcode_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
@@ -24,3 +24,18 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int count = 0;
+        for (auto i = grid.begin(); i != grid.end(); i++)
+            for (auto j = i->rbegin(); j != i->rend(); j++) {
+                if ((*j) < 0)
+                    count++;
+                else
+                    break;
+            }
+        return count;
+    }
+};
