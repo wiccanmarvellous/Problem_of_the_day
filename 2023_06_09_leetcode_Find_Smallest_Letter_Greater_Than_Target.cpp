0aa87b69ae7179adb931a5c1ec70b44c4b079664
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-09 19:00:51 +0530

    feat: create 2023_06_09_leetcode_Find_Smallest_Letter_Greater_Than_Target.cpp

---
+++ 2023_06_09_leetcode_Find_Smallest_Letter_Greater_Than_Target.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for (int i = 0; i < letters.size(); i++)
+            if (letters[i] > target)
+                return letters[i];
+        return letters[0];
+    }
+};
