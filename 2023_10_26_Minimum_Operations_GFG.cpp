7eee6240c9d517ca1367534cb57c0eeb45803235
Shreyash <shreyashyaduvanshi@gmail.com>
2023-10-26 20:56:41 +0530

    feat: create 2023_10_26_Minimum_Operations_GFG.cpp

---
+++ 2023_10_26_Minimum_Operations_GFG.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 0)
+                n /= 2;
+            else
+                n--;
+            count++;
+        }
+        return count;
+    }
+};
