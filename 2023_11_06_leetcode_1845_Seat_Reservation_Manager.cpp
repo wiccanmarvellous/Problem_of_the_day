d9536fe0201fe2d00fb8dbc4fe4a66373df28d05
Shreyash <shreyashyaduvanshi@gmail.com>
2023-11-06 21:02:46 +0530

    feat: create 2023_11_06_leetcode_1845_Seat_Reservation_Manager.cpp

---
+++ 2023_11_06_leetcode_1845_Seat_Reservation_Manager.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SeatManager {
+public:
+    set<int> s;
+    SeatManager(int n) {
+        for (int i = 1; i <= n; ++i)
+            s.insert(i);
+    }
+    int reserve() {
+        int res = *begin(s);
+        s.erase(begin(s));
+        return res;
+    }
+    void unreserve(int seatNumber) {
+        s.insert(seatNumber);
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
