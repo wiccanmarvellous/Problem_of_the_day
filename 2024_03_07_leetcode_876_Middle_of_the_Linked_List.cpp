e78fae2d2bbfb5399576d2ce35d5543e4d6ab6fd
Shreyash <shreyashyaduvanshi@gmail.com>
2024-03-07 23:42:19 +0530

    feat: create 2024_03_07_leetcode_876_Middle_of_the_Linked_List.cpp

---
+++ 2024_03_07_leetcode_876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == NULL)   return NULL;
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if (fast->next) return slow->next;
+        return slow;
+    }
+};
